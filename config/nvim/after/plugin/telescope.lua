7e8d97516fd8c1f74afd7adbe1ddefafd583f6c6
Bin Li <binli@gnome.org>
2024-04-08 12:21:27 +0800

    Support ESC to grep automatically

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function() builtin.grep_string({ search = vim.fn.input("Grep For > ")}) end)
+--vim.keymap.set('n', '<leader>fs', function() builtin.grep_string({ search = vim.fn.input("Grep For > ")}) end)
+vim.keymap.set('n', '<leader>fs', function() vim.ui.input({prompt = ' Grep > '}, function(value) require('telescope.builtin').grep_string({search = value}) end) end)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
