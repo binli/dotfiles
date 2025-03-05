3e3cdceb3aa9ba6cc545d27664b0f9c006b92948
Bin Li <binli@ubuntu.com>
2025-03-05 19:40:45 +0800

    Add python3Full

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,6 +95,7 @@
     google-chrome
     neovim
     pciutils
+    python3Full
     ripgrep
     tmux
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
