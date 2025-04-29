e40abdf01fdc8467893e998fa5d1565d4ff10877
Bin Li <binli@ubuntu.com>
2025-04-29 23:28:32 +0800

    update timezone

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,10 +25,22 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Asia/Hong_Kong";
+  time.timeZone = "Asia/Shanghai";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_HK.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
 
   i18n.inputMethod = {
     type = "fcitx5";
@@ -92,6 +104,10 @@
 
   # Install firefox.
   programs.firefox.enable = true;
+  programs.git.enable = true;
+  programs.git.prompt.enable = true;
+  programs.neovim.enable = true;
+  programs.vim.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -118,12 +134,7 @@
     smplayer
     tmux
   ];
-  programs.git.enable = true;
-  programs.git.prompt.enable = true;
-  programs.neovim.enable = true;
-  programs.vim.enable = true;
 
-  #programs.geary.enable = false;
   environment.gnome.excludePackages = with pkgs; [
     totem
     geary
