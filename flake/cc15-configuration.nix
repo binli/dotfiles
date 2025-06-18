4003298ef7e1ecaa5ef6873b86826b61c510b6d9
Bin Li <binli@ubuntu.com>
2025-06-18 22:16:45 +0800

    Add cjk fonts

--- flake/cc15-configuration.nix
+++ flake/cc15-configuration.nix
@@ -63,8 +63,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -155,6 +155,13 @@
     settings = {
       default-cache-ttl = 7200;
     };
+  };
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts-cjk-sans
+    ];
   };
 
   # List services that you want to enable:
