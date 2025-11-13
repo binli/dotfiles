2acbd22ee5f4fdf43673d8a4cbe0c38b693398dd
Bin Li <binli@ubuntu.com>
2025-11-13 12:35:28 +0800

    ssh config

--- flake/home.nix
+++ flake/home.nix
@@ -201,7 +201,7 @@
   };
   programs.ssh = {
     enable = true;
-    matchBlocks."*".userKnownHostsFile = "/dev/null";
+    userKnownHostsFile = "/dev/null";
     extraConfig =
     ''
     StrictHostKeyChecking no
